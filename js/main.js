ccbd05d5a3b86b1794dd1b4a8640a50c6b82c7f4
Renzo Zapata <renzozs2000@gmail.com>
2023-08-03 17:44:30 -0500

    Iconos, cambios, pisos

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { onMouseClick, addMarker} from './scripts';
-import { handleScreenSize } from "./scripts";
-import labelRenderer from "./scripts";
+import { onMouseClick, addMarker} from './scripts.js';
+import { handleScreenSize } from "./scripts.js";
+import labelRenderer from "./scripts.js";
 
 // Array de los objetos 3D seleccionables
-const objetos = ['Atencion_docente', 'Auditorio', 'Aula_NP1', 'Aula_NP2', 'Aulas_1001', 'Aulas_1002', 'Capilla', 'Cerseu', 'DGA', 'Direccion_escuela', 'Economia', 'Losa', 'Quiosco', 'SSHH1', 'SSHH2', 'USGOM', 'Vestidores'];
+const objetos=[{nombre:'Atencion_docente',icono:'teacher.png'},{nombre:'Auditorio',icono:'auditorio.png'},{nombre:'Aula_NP1',icono:'clase.png'},{nombre:'Aula_NP2',icono:'clase.png'},{nombre:'Aulas_1001',icono:'clase.png'},{nombre:'Aulas_1002',icono:'clase.png'},{nombre:'Capilla',icono:'Capilla.png'},{nombre:'Cerseu',icono:'teacher.png'},{nombre:'DGA',icono:'secretary.png'},{nombre:'Direccion_escuela',icono:'secretary.png'},{nombre:'Economia',icono:'secretary.png'},{nombre:'Losa',icono:'teacher.png'},{nombre:'Quiosco',icono:'shop color.png'},{nombre:'SSHH1',icono:'men´s room.png'},{nombre:'SSHH2',icono:'men´s room.png'},{nombre:'USGOM',icono:'secretary.png'},{nombre:'Vestidores',icono:'womens-room.png'}]
 
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
@@ -42,30 +42,34 @@ controls.enableZoom = true;
 controls.maxPolarAngle = Math.PI / 2;
 controls.update();
 
-// Agregar una luz direccional a la escena
-const light = new THREE.PointLight(0xffffff, 9, 8, 1);
-light.position.set(3, 3, -3);
-scene.add(light);
-// const dl = new THREE.PointLightHelper(light, 0.5);
-// scene.add(dl)
+function initLights() {
+    // Agregar una luz direccional a la escena
+    const light = new THREE.PointLight(0xffffff, 9, 8, 1);
+    light.position.set(3, 3, -3);
+    scene.add(light);
+    // const dl = new THREE.PointLightHelper(light, 0.5);
+    // scene.add(dl)
+    
+    const light2 = new THREE.PointLight(0xffffff, 9, 8, 1);
+    light2.position.set(-3, 3, 3);
+    scene.add(light2);
+    const dl2 = new THREE.PointLightHelper(light2, 0.5);
+    // scene.add(dl2)
+    
+    const light3 = new THREE.PointLight(0xffffff, 9, 8, 1);
+    light3.position.set(-3, 3, -3);
+    scene.add(light3);
+    // const dl3 = new THREE.PointLightHelper(light3, 0.5);
+    // scene.add(dl3)
+    
+    const light4 = new THREE.PointLight(0xffffff, 9, 16, 1);
+    light4.position.set(3, 3, 3);
+    scene.add(light4);
+    // const dl4 = new THREE.PointLightHelper(light4, 0.5);
+    // scene.add(dl4)
+}
 
-const light2 = new THREE.PointLight(0xffffff, 9, 8, 1);
-light2.position.set(-3, 3, 3);
-scene.add(light2);
-const dl2 = new THREE.PointLightHelper(light2, 0.5);
-// scene.add(dl2)
-
-const light3 = new THREE.PointLight(0xffffff, 9, 8, 1);
-light3.position.set(-3, 3, -3);
-scene.add(light3);
-// const dl3 = new THREE.PointLightHelper(light3, 0.5);
-// scene.add(dl3)
-
-const light4 = new THREE.PointLight(0xffffff, 9, 16, 1);
-light4.position.set(3, 3, 3);
-scene.add(light4);
-// const dl4 = new THREE.PointLightHelper(light4, 0.5);
-// scene.add(dl4)
+initLights();
 
 loader.load('../models/PrimerPiso.glb', function (gltf) {
     const pared = gltf.scene.children.find((child) => child.name === "Pared");
@@ -88,27 +92,222 @@ loader.load('../models/PrimerPiso.glb', function (gltf) {
 
 });
 
+/*
+// Variables para el camino
+let startPoint = new THREE.Vector3(1.278513789176941, 0.1900000125169754, -0.328339159488678);
+let endPoint = new THREE.Vector3(-1.5243076086044312, 0.1900000125169754, 1.954158067703247);
+let pathPoints = [];
+
+// Función para generar el camino
+function generatePath(glb) {
+    pathPoints = [];
+    let stepSize = 0.1; // Tamaño del paso para el raycasting
+    let direction = new THREE.Vector3().subVectors(endPoint, startPoint).normalize();
+    let distance = startPoint.distanceTo(endPoint);
+    let raycaster = new THREE.Raycaster();
+
+    let currentPoint = new THREE.Vector3().copy(startPoint);
+    let raycastIntersections = [];
+
+    while (currentPoint.distanceTo(startPoint) < distance) {
+        raycaster.set(currentPoint, direction);
+        raycastIntersections = raycaster.intersectObject(glb, true);
+        if (raycastIntersections.length > 0) {
+            var intersection = raycastIntersections[0];
+            pathPoints.push(intersection.point.clone());
+            currentPoint.copy(intersection.point).add(direction.multiplyScalar(stepSize));
+        } else {
+            // Si no hay colisión, avanzar al siguiente punto sin obstáculos
+            currentPoint.add(direction.multiplyScalar(stepSize));
+        }
+    }
+
+    // Agregar el punto final
+    pathPoints.push(endPoint.clone());
+
+    // Dibujar el camino (puedes usar el color o material que desees)
+    var pathMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    var pathGeometry = new THREE.BufferGeometry().setFromPoints(pathPoints);
+    var pathLine = new THREE.Line(pathGeometry, pathMaterial);
+    scene.add(pathLine);
+}
+*/
+
 loader.load('../models/PrimerPiso.glb', function (gltf) {
+    let i=0
     for (let objeto of objetos) {
-        const buildMesh = gltf.scene.children.find((child) => child.name === objeto);
+        const buildMesh = gltf.scene.children.find((child) => child.name === objeto.nombre);
 
         if (buildMesh) {
             const grayScale = Math.random(); // Generar valor de escala de grises aleatorio (entre 0 y 1)
             const color = new THREE.Color(grayScale, grayScale, grayScale); // Crear color en escala de grises
-            addMarker(scene, 'name', ...buildMesh.position);
+            addMarker(scene, objetos[i], ...buildMesh.position);
             buildMesh.material = new THREE.MeshPhysicalMaterial({
                 color: color, // Asignar color aleatorio al material
                 clearcoat: 1
             });
-
+            objetos[i].x=buildMesh.position.x;
+            objetos[i].y=buildMesh.position.y;
+            objetos[i].z=buildMesh.position.z;
             scene.add(buildMesh);
+            i++;
         } else {
             console.warn(`Objeto "${objeto}" no encontrado en el modelo.`);
         }
     }
+    //generatePath(gltf.scene)
 }, undefined, function (error) {
     console.error(error);
 });
+// Variables para controlar el zoom
+var zoomMin = 0.5; // Zoom mínimo permitido
+var zoomMax = 2.0; // Zoom máximo permitido
+var zoomSpeed = 0.000001; // Velocidad de zoom
+
+// Función para limitar el zoom de la cámara
+function limitZoom() {
+    if (camera.zoom < zoomMin) camera.zoom = zoomMin;
+    if (camera.zoom > zoomMax) camera.zoom = zoomMax;
+}
+
+// Función para manejar el evento de zoom
+function handleZoom(event) {
+    var delta = 0;
+    if (event.wheelDelta) { // Evento de rueda del ratón
+        delta = event.wheelDelta;
+    } else if (event.deltaY) { // Evento de rueda del ratón 
+        delta = event.deltaY * -1;
+    } else if (event.touches && event.touches.length === 2) { // Evento de pinch en dispositivos táctiles
+        var touch1 = event.touches[0];
+        var touch2 = event.touches[1];
+        delta = Math.sqrt(
+            Math.pow(touch2.clientX - touch1.clientX, 2) +
+            Math.pow(touch2.clientY - touch1.clientY, 2)
+        );
+    }
+    // Actualizar el zoom de la cámara
+    camera.zoom += delta * zoomSpeed;
+    limitZoom();
+    camera.updateProjectionMatrix();
+}
+// Escuchar los eventos de zoom
+window.addEventListener('wheel', handleZoom, false);
+window.addEventListener('touchmove', handleZoom, false);
+
+function cambiarPrimerPiso() {
+    loader.load('../models/PrimerPiso.glb', function (gltf) {
+        const pared = gltf.scene.children.find((child) => child.name === "Pared");
+        if (pared) {
+            pared.material = new THREE.MeshPhysicalMaterial({ color: 0x001657, clearcoat: 1 })
+            scene.add(pared);
+        } else {
+            console.warn('Pared no encontrada en el modelo.');
+        }
+        const base = gltf.scene.children.find((child) => child.name === "Base");
+        if (base) {
+            base.material = new THREE.MeshPhysicalMaterial({ color: 0xD8E9FF })
+            scene.add(base);
+        } else {
+            console.warn('Pared no encontrada en el modelo.');
+        }
+    }, undefined, function (error) {
+    
+        console.error(error);
+    
+    });
+
+    loader.load('../models/PrimerPiso.glb', function (gltf) {
+        let i=0
+        for (let objeto of objetos) {
+            const buildMesh = gltf.scene.children.find((child) => child.name === objeto.nombre);
+    
+            if (buildMesh) {
+                const grayScale = Math.random(); // Generar valor de escala de grises aleatorio (entre 0 y 1)
+                const color = new THREE.Color(grayScale, grayScale, grayScale); // Crear color en escala de grises
+                addMarker(scene, objetos[i], ...buildMesh.position);
+                buildMesh.material = new THREE.MeshPhysicalMaterial({
+                    color: color, // Asignar color aleatorio al material
+                    clearcoat: 1
+                });
+                objetos[i].x=buildMesh.position.x;
+                objetos[i].y=buildMesh.position.y;
+                objetos[i].z=buildMesh.position.z;
+                scene.add(buildMesh);
+                i++;
+            } else {
+                console.warn(`Objeto "${objeto}" no encontrado en el modelo.`);
+            }
+        }
+        //generatePath(gltf.scene)
+    }, undefined, function (error) {
+        console.error(error);
+    });
+}
+
+// Función para cambiar a segundo piso
+function cambiarSegundoPiso() {
+    // Remover todos los objetos hijos de la escena (limpiar el primer piso)
+    while (scene.children.length > 0) {
+        scene.remove(scene.children[0]);
+    }
+    initLights();
+    loader.load('../models/PrimerPiso.glb', function (gltf) {
+        const pared = gltf.scene.children.find((child) => child.name === "Pared");
+        if (pared) {
+            pared.material = new THREE.MeshPhysicalMaterial({ color: 0x001657, clearcoat: 1 })
+            scene.add(pared);
+        } else {
+            console.warn('Pared no encontrada en el modelo.');
+        }
+        const base = gltf.scene.children.find((child) => child.name === "Base");
+        if (base) {
+            base.material = new THREE.MeshPhysicalMaterial({ color: 0xD8E9FF })
+            scene.add(base);
+        } else {
+            console.warn('Pared no encontrada en el modelo.');
+        }
+    }, undefined, function (error) {
+    
+        console.error(error);
+    
+    });
+}
+
+// Asociar evento de clic al botón
+const changeFloorButton1 = document.getElementById("changeFloorButton1");
+changeFloorButton1.addEventListener("click", function(event) {
+    event.stopPropagation(); // Detener la propagación del evento de clic
+    cambiarPrimerPiso();
+});
+
+// Asociar evento de clic al botón
+const changeFloorButton2 = document.getElementById("changeFloorButton2");
+changeFloorButton2.addEventListener("click", function(event) {
+    event.stopPropagation(); // Detener la propagación del evento de clic
+    cambiarSegundoPiso();
+});
+
+/*let infoVisible = false;
+// Función para mostrar el card con información
+function showInfoCard(nombre) {
+    if (infoVisible == false) {
+        infoVisible = true
+        $('#informacion').toggle('slide', { direction: 'right' }, 1000);
+        $('#canvas-container').css('transform', 'translateX(-20%)');
+    }
+    $("#titulo").text(nombre)
+
+}
+
+// Función para ocultar el card con información
+function hideInfoCard() {
+    if (infoVisible == true) {
+        infoVisible = false
+        $('#informacion').toggle('slide', { direction: 'right' }, 1000);
+        $('#canvas-container').css('transform', 'translateX(0%)');
+    }
+
+}*/
 
 // Añadimos el evento de clic del mouse al documento
 document.addEventListener('click', (event) => {
@@ -128,7 +327,7 @@ function animate() {
     // Renderizamos la escena
     controls.update();
     labelRenderer.render(scene, camera);
-    handleScreenSize();
+    handleScreenSize()
     renderer.render(scene, camera);
 }
 
--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer
 let SELECTED = null;
 let initColor = null;
 let infoVisible = false;
+// let infoContainer; // Declarar la variable a nivel del módulo
+// let infoContainer2; // Declarar la variable a nivel del módulo
 
 // Raycaster para detectar colisiones
 const raycaster = new THREE.Raycaster();
-let mouse = new THREE.Vector2();
+const mouse = new THREE.Vector2();
 const container = document.getElementById('canvas-container');
 const labelRenderer = new CSS2DRenderer()
 labelRenderer.setSize(window.innerWidth, window.innerHeight);
@@ -74,7 +76,8 @@ export function addMarker(scene, name, x, y, z) {
     const div = document.createElement("div");
     div.classList.add('point')
     div.innerHTML = `
-        <img class="label icn" src="../images/${name}.svg" alt="${name}">
+        <img class="label icn" src="../images/${name.icono}" alt="${name}">
+        <p class="text">${name.nombre}</p>
     `
     const cPointLabel = new CSS2DObject(div);
     scene.add(cPointLabel);
@@ -88,7 +91,6 @@ export function hideMarkers() {
         div.style.opacity = "0"; // Cambia este valor según lo necesites
     }
 }
-
 export function showMarkers() {
     const divs = document.getElementsByClassName("label");
     // Cambia la opacidad de todos los elementos
@@ -96,7 +98,6 @@ export function showMarkers() {
         div.style.opacity = "1"; // Cambia este valor según lo necesites
     }
 }
-
 // Función para deshabilitar la interacción con objetos de la escena
 function disableInteraction() {
     mouse = null;
@@ -150,4 +151,37 @@ function hideInfoCard() {
 
 // Llama a la función cuando se carga la página y cuando cambia el tamaño de la ventana
 window.addEventListener('resize', handleScreenSize);
+
+// Elemento HTML para mostrar el card con información
+// export function createHelperContainer() {
+//     infoContainer2 = document.createElement("div");
+//     infoContainer2.style.position = "absolute";
+//     infoContainer2.style.top = "10px";
+//     infoContainer2.style.left = window.innerWidth; // Ajusta el valor aquí para determinar el espaciado
+//     infoContainer2.style.width = "20%"
+//     infoContainer2.style.pointerEvents = "none";
+//     document.body.appendChild(infoContainer2);
+//     showHelper();
+// }
+
+// function showHelper() {
+//     infoContainer2.innerHTML = `
+//     <div class="card info-card">
+//       <div class="card-body">
+//         <h5 class="card-title">Bienvenido!</h5>
+//         <p class="card-text">Aquí encontrará todas las dependencias de la FISI</p>
+//         <p class="card-footer">Seleccione cualquier marcador u objeto para ver más información</p>
+//       </div>
+//     </div>`
+// }
+
+// Elemento HTML para mostrar el card con información
+// export function createCardContainer() {
+//     infoContainer = document.createElement("div");
+//     infoContainer.style.position = "absolute";
+//     infoContainer.style.top = "10px";
+//     infoContainer.style.left = window.innerWidth - 300 + "px"; // Ajusta el valor aquí para determinar el espaciado
+//     infoContainer.style.pointerEvents = "none";
+//     document.body.appendChild(infoContainer);
+// }
 

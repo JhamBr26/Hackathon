491f0da9926bd1d818845b38af38a405c8dc7ce0
Renzo Zapata <renzozs2000@gmail.com>
2023-08-03 04:19:08 -0500

    Template añadida

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { onMouseClick, addMarker, createCardContainer, createHelperContainer } from './scripts';
-import labelRenderer from './scripts.js';
+import { onMouseClick, addMarker} from './scripts';
+import labelRenderer from "./scripts";
 
 // Array de los objetos 3D seleccionables
 const objetos = ['Atencion_docente', 'Auditorio', 'Aula_NP1', 'Aula_NP2', 'Aulas_1001', 'Aulas_1002', 'Capilla', 'Cerseu', 'DGA', 'Direccion_escuela', 'Economia', 'Losa', 'Quiosco', 'SSHH1', 'SSHH2', 'USGOM', 'Vestidores'];
@@ -11,7 +11,10 @@ const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x171819); // Cambia el color de fondo de la escena a azul
 
-const aspect = window.innerWidth / window.innerHeight;
+// Se define el div que contiene el canva
+const container = document.getElementById('canvas-container');
+
+const aspect = container.clientWidth / container.clientHeight;
 const frustumSize = 4; // Ajusta este valor según tus necesidades
 
 const camera = new THREE.OrthographicCamera(
@@ -29,7 +32,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // Aplicar las configuraciones adicionales al renderer
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-document.body.appendChild(renderer.domElement);
+container.appendChild(renderer.domElement);
 
 // Iniciar controles de la cámara
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -63,20 +66,18 @@ scene.add(light4);
 // const dl4 = new THREE.PointLightHelper(light4, 0.5);
 // scene.add(dl4)
 
-// Agregar el plano base
-const geometry = new THREE.BoxGeometry(10, 10, 10);
-const material = new THREE.MeshPhysicalMaterial({ color: 0xD8E9FF })
-const plano = new THREE.Mesh(geometry, material);
-plano.name = 'Plano'
-plano.position.set(0, 0, 0);
-plano.scale.set(0.5, 0.01, 0.5)
-scene.add(plano);
-
 loader.load('../models/PrimerPiso.glb', function (gltf) {
     const pared = gltf.scene.children.find((child) => child.name === "Pared");
     if (pared) {
         pared.material = new THREE.MeshPhysicalMaterial({ color: 0x001657, clearcoat: 1 })
         scene.add(pared);
+    } else {
+        console.warn('Pared no encontrada en el modelo.');
+    }
+    const base = gltf.scene.children.find((child) => child.name === "Base");
+    if (base) {
+        base.material = new THREE.MeshPhysicalMaterial({ color: 0xD8E9FF })
+        scene.add(base);
     } else {
         console.warn('Pared no encontrada en el modelo.');
     }
@@ -108,13 +109,32 @@ loader.load('../models/PrimerPiso.glb', function (gltf) {
     console.error(error);
 });
 
-createHelperContainer(document);
-createCardContainer(document);
+let infoVisible = false;
+// Función para mostrar el card con información
+function showInfoCard(nombre) {
+    if (infoVisible == false) {
+        infoVisible = true
+        $('#informacion').toggle('slide', { direction: 'right' }, 1000);
+        $('#canvas-container').css('transform', 'translateX(-20%)');
+    }
+    $("#titulo").text(nombre)
+
+}
+
+// Función para ocultar el card con información
+function hideInfoCard() {
+    if (infoVisible == true) {
+        infoVisible = false
+        $('#informacion').toggle('slide', { direction: 'right' }, 1000);
+        $('#canvas-container').css('transform', 'translateX(0%)');
+    }
+
+}
 
 // Añadimos el evento de clic del mouse al documento
 document.addEventListener('click', (event) => {
     // Llamada a la función onMouseClick pasando las funciones showInfoCard y hideInfoCard
-    onMouseClick(event, camera, scene, controls);
+    onMouseClick(event, camera, scene, showInfoCard, hideInfoCard, controls);
 }, false);
 
 window.addEventListener('resize', function () {
--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,28 @@ import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer
 // Variable para almacenar el objeto actualmente seleccionado
 let SELECTED = null;
 let initColor = null;
-let infoContainer; // Declarar la variable a nivel del módulo
-let infoContainer2; // Declarar la variable a nivel del módulo
+// let infoContainer; // Declarar la variable a nivel del módulo
+// let infoContainer2; // Declarar la variable a nivel del módulo
 
 // Raycaster para detectar colisiones
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-
+const container = document.getElementById('canvas-container');
 const labelRenderer = new CSS2DRenderer()
 labelRenderer.setSize(window.innerWidth, window.innerHeight);
 labelRenderer.domElement.style.position = 'absolute';
 labelRenderer.domElement.style.top = '0px';
 labelRenderer.domElement.style.pointerEvents = 'none';
-document.body.appendChild(labelRenderer.domElement);
+container.appendChild(labelRenderer.domElement);
 export default labelRenderer;
 
 // Función para manejar los clics del mouse
-export function onMouseClick(event, camera, scene) {
-    // Calculamos la posición normalizada del mouse (-1 a 1)
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+export function onMouseClick(event, camera, scene, showInfoCard, hideInfoCard) {
+    // Nueva posición segun el div canvas-container
+    const container = document.getElementById('canvas-container');
+    const rect = container.getBoundingClientRect();
+    mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
 
     // Lanzamos un rayo desde la posición del mouse
     raycaster.setFromCamera(mouse, camera);
@@ -35,7 +37,7 @@ export function onMouseClick(event, camera, scene) {
         // Si el rayo intersecta con algún objeto de la escena
         const clickedObject = intersects[0].object;
         console.log(clickedObject.position)
-        if (clickedObject.name !== 'Pared' && clickedObject.name !== 'Plano') {
+        if (clickedObject.name !== 'Pared' && clickedObject.name !== 'Base') {
 
             if (SELECTED !== clickedObject) {
                 if (SELECTED) {
@@ -50,7 +52,7 @@ export function onMouseClick(event, camera, scene) {
                 showInfoCard(nombre);
 
                 // Cambiamos el color del objeto seleccionado
-                SELECTED.material.color.set(0xFFDD47);
+                SELECTED.material.color.set(0xF13A12);
             } else {
                 // Hacer clic nuevamente en el objeto seleccionado para deseleccionarlo
                 SELECTED.material.color.set(initColor);
@@ -78,63 +80,38 @@ export function addMarker(scene, name, x, y, z) {
     const cPointLabel = new CSS2DObject(div);
     scene.add(cPointLabel);
     cPointLabel.position.set(x, y + 0.3, z);
-
-    // Agregar un evento de clic al marcador
-    cPointLabel.element.addEventListener('click', () => {
-        showInfoCard(name); // Mostrar el card correspondiente
-    });
 }
 
 // Elemento HTML para mostrar el card con información
-export function createHelperContainer() {
-    infoContainer2 = document.createElement("div");
-    infoContainer2.style.position = "absolute";
-    infoContainer2.style.top = "10px";
-    infoContainer2.style.left = window.innerWidth; // Ajusta el valor aquí para determinar el espaciado
-    infoContainer2.style.width = "20%"
-    infoContainer2.style.pointerEvents = "none";
-    document.body.appendChild(infoContainer2);
-    // showHelper();
-}
+// export function createHelperContainer() {
+//     infoContainer2 = document.createElement("div");
+//     infoContainer2.style.position = "absolute";
+//     infoContainer2.style.top = "10px";
+//     infoContainer2.style.left = window.innerWidth; // Ajusta el valor aquí para determinar el espaciado
+//     infoContainer2.style.width = "20%"
+//     infoContainer2.style.pointerEvents = "none";
+//     document.body.appendChild(infoContainer2);
+//     showHelper();
+// }
 
-function showHelper() {
-    infoContainer2.innerHTML = `
-    <div class="card info-card">
-      <div class="card-body">
-        <h5 class="card-title">Bienvenido!</h5>
-        <p class="card-text">Aquí encontrará todas las dependencias de la FISI</p>
-        <p class="card-footer">Seleccione cualquier marcador u objeto para ver más información</p>
-      </div>
-    </div>`
-}
+// function showHelper() {
+//     infoContainer2.innerHTML = `
+//     <div class="card info-card">
+//       <div class="card-body">
+//         <h5 class="card-title">Bienvenido!</h5>
+//         <p class="card-text">Aquí encontrará todas las dependencias de la FISI</p>
+//         <p class="card-footer">Seleccione cualquier marcador u objeto para ver más información</p>
+//       </div>
+//     </div>`
+// }
 
 // Elemento HTML para mostrar el card con información
-export function createCardContainer() {
-    infoContainer = document.createElement("div");
-    infoContainer.style.position = "absolute";
-    infoContainer.style.top = "10px";
-    infoContainer.style.left = window.innerWidth - 300 + "px"; // Ajusta el valor aquí para determinar el espaciado
-    infoContainer.style.pointerEvents = "none";
-    document.body.appendChild(infoContainer);
-}
+// export function createCardContainer() {
+//     infoContainer = document.createElement("div");
+//     infoContainer.style.position = "absolute";
+//     infoContainer.style.top = "10px";
+//     infoContainer.style.left = window.innerWidth - 300 + "px"; // Ajusta el valor aquí para determinar el espaciado
+//     infoContainer.style.pointerEvents = "none";
+//     document.body.appendChild(infoContainer);
+// }
 
-// Función para mostrar el card con información
-function showInfoCard(nombre) {
-    infoContainer2.innerHTML = ''
-    infoContainer.innerHTML = `
-  <div class="card info-card">
-    <img src="https://estudiaperu.pe/wp-content/uploads/2019/10/Estudiar-carrera-de-Econom%C3%ADa-en-Per%C3%BA_opt.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Área de ${nombre}</h5>
-      <p class="card-text">Área privada de la facultad</p>
-      <p class="card-footer">Abre a las 8:00 am</p>
-    </div>
-  </div>
-`;
-}
-
-// Función para ocultar el card con información
-function hideInfoCard() {
-    infoContainer.innerHTML = "";
-    showHelper();
-}
